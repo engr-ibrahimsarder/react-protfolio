3ebefaea40befbce075f0f60790b1f456a4afbf4
@ibrahimsarder96 <ibrahimsarder969@gmail.com>
2023-11-06 23:24:01 +0600

    image background change

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -7,11 +7,11 @@ const About = () => {
     <div class="hero min-h-screen">
     <div class="hero-content flex-col lg:flex-row-reverse">
       <div id='bg' className='w-6/12 animate__animated animate__fadeInDown'>
-      <img style={{width:'100%'}} src={photo} alt=''></img>
+      <img style={{width:'500px', height: '300px', borderRadius: '0px 0px 80% 80%'}} src={photo} alt=''></img>
       </div>
       <div className='w-6/12 animate__animated animate__fadeInUp lg:pr-44'>
         <h1 class="text-xl text-justify text-amber-400 my-5">
-        My name is Ibrahim Sarder, and I'm a tech-savvy teenager with a strong interest in programming. I am passionate in providing solutions that improve people's lives while also challenging me. Currently enhancing my abilities as a Front-End and Back-End developer.</h1>
+        My name is Ibrahim Sarder and I'm a tech-savvy teenager with a strong interest in programming. I am passionate in providing solutions that improve people's lives while also challenging me. Currently enhancing my abilities as a Front-End and Back-End developer.</h1>
 
         <h2 class="text-xl text-justify text-amber-400 my-5">I develop websites and applications using HTML, CSS, and JavaScript. I am familiar with developing with provided layouts. I'm always improving myself with each project being put in my hands.</h2>
 
--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -8,13 +8,13 @@ const Banner = () => {
     <div class="hero min-h-screen">
     <div class="hero-content flex-col lg:flex-row-reverse">
       <div id='bg' className='animate__animated animate__backInRight'>
-      <img style={{width:'100%'}} src={photo} alt=''></img>
+      <img style={{width:'500px', height: '300px', borderRadius: '0px 0px 80% 80%'}} src={photo} alt=''></img>
       </div>
       <div className='animate__animated animate__backInLeft lg:pr-44'>
         <h1 class="text-6xl font-bold text-amber-400">Hi</h1>
         <h2 class="py-3 text-6xl font-bold text-amber-400">I'm Ibrahim</h2>
-        <h3  class="py-3 text-5xl font-bold"><span id='effect'>Web developer</span> </h3>
-        <h2 class="py-3 text-xl text-gray-300">I'm a passionate web developer with a focus on web development with JavaScript.</h2>
+        <h3  class="py-3 text-5xl font-bold"><span id='effect'>Software Engineer</span> </h3>
+        <h2 class="py-3 text-xl text-gray-300">I'm a passionate Software developer with a focus on Software development with JavaScript / C# / C / C++ and Python.</h2>
         <Link to='/contact' class="btn btn-wide btn-outline btn-warning text-xl font-bold" data-micron="bounce">Contact</Link>
       </div>
     </div>
